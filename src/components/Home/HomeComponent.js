13987d2214c9224da43c0a1760ad58f7bedf510a
Thomas McAdams <rithonn21@gmail.com>
2021-10-24 12:34:09 -0400

    Started work on home pagE

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -1,12 +1,34 @@
 import Container from "react-bootstrap/esm/Container";
 import { NavBarComponent } from "../navbar/Navbar";
+import {Button, Card, CardGroup} from "react-bootstrap";
+import {useState} from "react";
+
+function VideoGameCard(props) {
+    return(
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    {props.theNumber}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    )
+}
 
 export function HomeComponent(){
+    const arrayCount = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+    const arrayCountList = arrayCount.map((theNumber) => <VideoGameCard theNumber={theNumber}/>);
+
     return(
         <>
             <NavBarComponent/>
             <Container>
-                <h1>Home component</h1>
+                <CardGroup>
+                    {arrayCountList}
+                </CardGroup>
             </Container>
         </>
         
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,11 +7,11 @@ export function NavBarComponent(){
     return (
         <Navbar bg="dark" expand="lg" variant='dark'>
         <Container>
-          <Navbar.Brand href="#home">Tank Tracker <img class="tanksLogo" src={TankLogo} alt=''/></Navbar.Brand>
+          <Navbar.Brand href="#home">Video Game Tracker <img class="tanksLogo" src={TankLogo} alt=''/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">All Tanks</Nav.Link>
+            <Nav.Link href="#features">All Video Games</Nav.Link>
           </Nav>
 
           <Nav>
